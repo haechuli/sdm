e74a040081abf74d95650f087192cf10a3228b33
jung hae chul <haechuli@empal.com>
2025-07-08 09:11:16 +0900

    add logging

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,12 @@ import { provideRouter } from '@angular/router';
 import { provideHttpClient ,withInterceptors} from '@angular/common/http';
 import { errorInterceptor } from './core/interceptor/error.interceptor';
 import { jwtInterceptor } from './core/interceptor/auth.interceptor';
+import { loggingInterceptor } from './core/interceptor/logging.interceptor';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), 
-              provideRouter(routes), 
-              provideHttpClient(withInterceptors([jwtInterceptor,errorInterceptor]))
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
+              provideRouter(routes),
+              provideHttpClient(withInterceptors([jwtInterceptor,errorInterceptor,loggingInterceptor]))
             ]
 };
