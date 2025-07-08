4c27268e8c8977a69b30f60002faefbfe0a14922
jung hae chul <haechuli@empal.com>
2025-07-09 00:59:38 +0900

    add combobox

--- src/app/shared/ui-component/combobox/combobox.component.ts
+++ src/app/shared/ui-component/combobox/combobox.component.ts
@@ -1,12 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter, forwardRef } from '@angular/core';
+import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
+
+export interface ComboOption {
+  label: string;
+  value: any;
+}
 
 @Component({
-  selector: 'ti-combobox',
   standalone: true,
-  imports: [],
+  selector: 'app-combobox',
   templateUrl: './combobox.component.html',
-  styleUrl: './combobox.component.scss'
+  styleUrls: ['./combobox.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => ComboBoxComponent),
+      multi: true
+    }
+  ]
 })
-export class ComboboxComponent {
+export class ComboBoxComponent implements ControlValueAccessor {
+  @Input() options: ComboOption[] = [];
+  @Input() placeholder: string = 'Select an option';
+  @Input() disabled: boolean = false;
+  @Output() selectionChange = new EventEmitter<any>();
 
+  value: any = null;
+
+  onChange = (value: any) => {};
+  onTouched = () => {};
+
+  writeValue(value: any): void {
+    this.value = value;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
+
+  onSelectChange(event: Event) {
+    const selectedValue = (event.target as HTMLSelectElement).value;
+    this.value = selectedValue;
+    this.onChange(selectedValue);
+    this.selectionChange.emit(selectedValue);
+  }
 }
